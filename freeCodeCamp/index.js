e62d02ddf84769cac8d401047185b395e41680bd
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-05 17:58:34 +0530

    Defining custom types

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -1,41 +1,47 @@
-"use strict";
 const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Vegetarian", price: 9 },
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Vegetarian", price: 9 },
 ];
+
 let cashInRegister = 100;
 let nextOrderId = 1;
 const orderQueue = [];
+
 function addNewPizza(pizzaObj) {
-    menu.push(pizzaObj);
+  menu.push(pizzaObj);
 }
-function placeOrder(pizzaName) {
-    const selectedPizza = menu.find((item) => item.name === pizzaName);
-    if (!selectedPizza) {
-        console.error(`${pizzaName} is not available on the menu.`);
-        return;
-    }
-    cashInRegister += selectedPizza.price;
-    const newOrder = {
-        id: nextOrderId++,
-        pizza: selectedPizza,
-        status: "ordered",
-    };
-    orderQueue.push(newOrder);
-    return newOrder;
+
+function placeOrder(pizzaName:string) {
+  const selectedPizza = menu.find((item) => item.name === pizzaName);
+  if (!selectedPizza) {
+    console.error(`${pizzaName} is not available on the menu.`);
+    return;
+  }
+  cashInRegister += selectedPizza.price;
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
 }
-function completeOrder(orderId) {
-    const order = orderQueue.find((order) => order.id === orderId);
-    order.status = "completed";
-    return order;
+
+function completeOrder(orderId: number) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = "completed";
+  return order;
 }
+
 addNewPizza({ name: "BBQ Chicken", price: 12 });
 addNewPizza({ name: "Chicken bacon ranch", price: 12 });
 addNewPizza({ name: "Spicy sausage", price: 11 });
+
 placeOrder("Chicken bacon ranch");
 completeOrder(1);
+
 console.log("Menu:", menu);
 console.log("Cash in Register:", cashInRegister);
 console.log("Order Queue:", orderQueue);
--- freeCodeCamp/index.ts
+++ freeCodeCamp/index.ts
@@ -1,47 +1,20 @@
-const menu = [
-  { name: "Margherita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Vegetarian", price: 9 },
-];
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
 
-let cashInRegister = 100;
-let nextOrderId = 1;
-const orderQueue = [];
+let person1: Person = {
+  name: "joe",
+  age: 42,
+  isStudent: true,
+};
+let person2: Person = {
+  name: "jill",
+  age: 64,
+  isStudent: false,
+};
 
-function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj);
-}
+type Food = string;
 
-function placeOrder(pizzaName:string) {
-  const selectedPizza = menu.find((item) => item.name === pizzaName);
-  if (!selectedPizza) {
-    console.error(`${pizzaName} is not available on the menu.`);
-    return;
-  }
-  cashInRegister += selectedPizza.price;
-  const newOrder = {
-    id: nextOrderId++,
-    pizza: selectedPizza,
-    status: "ordered",
-  };
-  orderQueue.push(newOrder);
-  return newOrder;
-}
-
-function completeOrder(orderId: number) {
-  const order = orderQueue.find((order) => order.id === orderId);
-  order.status = "completed";
-  return order;
-}
-
-addNewPizza({ name: "BBQ Chicken", price: 12 });
-addNewPizza({ name: "Chicken bacon ranch", price: 12 });
-addNewPizza({ name: "Spicy sausage", price: 11 });
-
-placeOrder("Chicken bacon ranch");
-completeOrder(1);
-
-console.log("Menu:", menu);
-console.log("Cash in Register:", cashInRegister);
-console.log("Order Queue:", orderQueue);
+let favoriteFood: Food = "pizza";
