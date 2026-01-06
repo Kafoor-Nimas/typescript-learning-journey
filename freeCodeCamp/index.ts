00edd367b71e95f38f5f0daa6648efb145877c2d
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-06 17:46:22 +0530

    Unions

--- freeCodeCamp/index.ts
+++ freeCodeCamp/index.ts
@@ -1,2 +1,11 @@
 let myName = "Bob";
-const myName2: "Bob" = "Bob"
+const myName2: "Bob" = "Bob";
+
+type user = {
+  username: string;
+  role: "guest" | "member" | "admin";
+};
+
+type UserRole = "guest" | "member" | "admin";
+
+let user2: UserRole = "member";
