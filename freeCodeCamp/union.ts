13f7f22557a6f4da4941f28b0b1ada792ebeb86a
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-07 16:39:59 +0530

    Omit Utility type

--- freeCodeCamp/union.ts
+++ freeCodeCamp/union.ts
@@ -9,15 +9,17 @@ type User = {
 
 type UpdatedUser = Partial<User>;
 
+let nextUserId = 1;
+
 type UserRole = "guest" | "member" | "admin";
 
 let user2: UserRole = "member";
 
 const users: User[] = [
-  { id: 1, username: "john_doe", role: "member" },
-  { id: 2, username: "jane_smith", role: "contributor" },
-  { id: 3, username: "alice_jones", role: "admin" },
-  { id: 4, username: "charlie_brown", role: "member" },
+  { id: nextUserId++, username: "john_doe", role: "member" },
+  { id: nextUserId++, username: "jane_smith", role: "contributor" },
+  // { id: 3, username: "alice_jones", role: "admin" },
+  // { id: 4, username: "charlie_brown", role: "member" },
 ];
 
 function updateUser(id: number, updates: UpdatedUser) {
@@ -30,5 +32,19 @@ function updateUser(id: number, updates: UpdatedUser) {
 }
 
 //example updates:
-updateUser(1, { username: "john_cena" });
-updateUser(4, { role: "contributor" });
+// updateUser(1, { username: "john_cena" });
+// updateUser(4, { role: "contributor" });
+
+function addNewUser(newUser: Omit<User, "id" | "user">): User {
+  const user: User = {
+    id: nextUserId++,
+    ...newUser,
+  };
+  users.push(user);
+  return user;
+}
+
+//example usage;
+addNewUser({ username: "joe_schmoe", role: "member" });
+
+console.log(users);
