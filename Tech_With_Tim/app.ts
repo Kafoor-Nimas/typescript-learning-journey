dbc012d5ce3cdff3edea3daa696e451002385b85
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-09 19:59:39 +0530

    Optional Chaining and bang

--- Tech_With_Tim/app.ts
+++ Tech_With_Tim/app.ts
@@ -1,12 +1,4 @@
-let x: any = 1;
-x.length;
+const arr = [{ name: "tim" }, { name: "Joe" }, { name: "Jane" }];
 
-let y: unknown = 1;
-if (typeof y === "number") {
-  const result = x + 1;
-} else if (typeof x == "string") {
-  const result = x.length;
-}
-
-// const result = y+1
-const result = (y as number) + 1;
+const elemnt = arr.pop()!.name
+const elemnt2 = arr.pop()?.name
