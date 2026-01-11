5c9aa50098053a08b97f5ba5a4d359266a00d173
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-11 15:25:57 +0530

    complex typescript functions

--- Tech_With_Tim/app.ts
+++ Tech_With_Tim/app.ts
@@ -1,15 +1,19 @@
-function add(x: number, y: number): number | string {
-  if (x == 0) {
-    return "invalid";
+function mul(x: number, y: number): number {
+  return x * y;
+}
+function div(x: number, y: number): number {
+  return x / y;
+}
+
+function applyFunc(funcs: ((a:number,b:number)=>number)[], values:[number,number][]): number[]{
+  const results = [] as number[];
+  for(let i=0;i<funcs.length;i++){
+    const args=values[i];
+    const result=funcs[i](args[0],args[1]);
+    results.push(result);
+
   }
-  return x + y;
+  return results;
 }
 
-const result = add(1, 2);
-
-function makeName(firstName:string,lastName:string,middleName?:string){
-
-}
-
-
-const fullName = makeName("Kafoor","Nimas") 
+applyFunc([mul,div],[[1,2],[4,5]]);
